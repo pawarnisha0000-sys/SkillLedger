52b56ab90adaedb5d2f8dc451f0e624353f40357
pawarnisha0000-sys <pawarnisha0000@gmail.com>
2026-04-15 22:31:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -256,6 +256,8 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(5000, () => {
-  console.log("🚀 SkillLedger Pro Backend running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
